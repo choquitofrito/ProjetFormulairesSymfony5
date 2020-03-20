2ded598cb57760869acf5e2b3502344e18187cb4
choquitofrito <zyriab@gmail.com>
2020-03-20 13:50:13 +0100

    ok après ajax

--- src/Controller/ExemplesAjaxAxiosController.php
+++ src/Controller/ExemplesAjaxAxiosController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-
-// entité Livre
 use App\Entity\Livre;
-// classe du formulaire
 use App\Form\LivreType;
 
+// entité Livre
+use App\Entity\Aeroport;
+// classe du formulaire
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ExemplesAjaxAxiosController extends AbstractController
 {
@@ -85,38 +87,61 @@ class ExemplesAjaxAxiosController extends AbstractController
     }
 
 
-    // AJAX avec des arrays d'objets
-    //    
-    //    
-
+    
     /**
-     * @Route ("/exemples/ajax/axios/exemple/renvoi/entite");
+     * @Route("/exemples/ajax/axios/exemple/affichage/objets/dql")
      */
-
-    // on affiche un input pour chercher les livres qui portent un titre
-    public function exempleRenvoiEntite()
+    public function exempleAffichageObjetsDql()
     {
-        return $this->render("/exemples_ajax_axios/exemple_renvoi_entite.html.twig");
+        return $this->render('/exemples_ajax_axios/exemple_affichage_objets_dql.html.twig');
     }
 
     /**
-     * @Route ("/exemples/ajax/axios/exemple/renvoi/entite/traitement",name ="renvoi_entite_traitement");
+     * @Route("/exemples/ajax/axios/exemple/affichage/objets/repo")
      */
-
-    // action qui traite une commande AJAX, elle a une vue associée
-    public function exempleRenvoiEntiteTraitementAjax(Request $requeteAjax)
+    public function exempleAffichageObjetsRepo()
     {
-        // on obtient ce qui se trouve dans l'input
-        $titre = $requeteAjax->get('titre');
-        $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery("SELECT livre FROM App\Entity\Livre livre WHERE" .
-            " livre.titre LIKE :titre");
-        $query->setParameter('titre', '%' . $titre . '%');
-        // dd($titre); // il faut vérifier qu'on reçoit le titre!
-        // $resultat = $query->getResult();
-        // dump ($resultat);
-        // die();
+        return $this->render('/exemples_ajax_axios/exemple_affichage_objets_repo.html.twig');
+    }
 
+
+    // 1. action de traitement du AJAX, on utilise les méthodes du repository (findBy, findAll, etc...)
+    // nous devons serialiser le résultat (le transformer en json dans ce cas) et envoyer une Reponse normale
+
+    /**
+     * @Route("/exemples/ajax/axios/exemple/affichage/objets/traitement/repo", name="exemple_objets_traitement_repo")
+     */
+    public function exempleAffichageObjetsTraitementRepo(Request $req)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $rep = $em->getRepository(Aeroport::class);
+        $code = $req->get('code');
+        $aeroports = $rep->findBy(['code' => $code]); // renvoie un array même s'il y a un seul objet
+        // Si on utilise les méthodes de base du Repository (find, findBy, findAll...)
+        // nous devons serializer à la main en utilisant la méthode "serialize", puis 
+        // envoyer une réponse normale. En DQL on peut utiliser getArrayResult, mais pas ici  
+        $aeroports = $this->get('serializer')->serialize($aeroports, 'json');
+        return new Response($aeroports);
+    }
+
+
+    // 2. action de traitement du AJAX, on utilise DQL
+    // La méthode getArrayResult créera un array manipulable par JsonResponse
+    // Dans ce cas on renvoie un JsonResponse au lieu d'une Response
+
+    /**
+     * @Route("/exemples/ajax/axios/exemple/affichage/objets/traitement/dql", name="exemple_objets_traitement_dql")
+     */
+    public function exempleAffichageObjetsTraitementDql(Request $req)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery("SELECT aeroport FROM App\Entity\Aeroport aeroport WHERE aeroport.code LIKE :code");
+        $code = $req->get('code');
+        $query->setParameter("code", '%' . $code . '%');
+
+        
         // avec getResult() on obtient un array contenant toutes les entités Livre 
         // qui contiennent dans son titre le texte saisi dans l'input
 
@@ -127,7 +152,8 @@ class ExemplesAjaxAxiosController extends AbstractController
         // contenant (dans ce cas) la réprésentation d'array de chaque entité 
         // contenant uniquement les propriétés de base propres à l'objet 
         // (pas les "rélations" ni d'autres propriétés)
-        $livresEnArray = $query->getArrayResult();
+        $aeroports = $query->getArrayResult();
+
         // Pour mieux comprendre faites un dump ici et regardez la 
         // réponse du serveur. 
 
@@ -137,6 +163,16 @@ class ExemplesAjaxAxiosController extends AbstractController
         // echo ici, car votre réponse ne sera plus du pur JSON
         // dump ($objetLivre);
 
-        return new JsonResponse($livresEnArray);
+        return new JsonResponse($aeroports);
+
+        // SELECT aeroport FROM App\Entity\Aeroport aeroport WHERE aeroport.code = 'CLR'
+        // SELECT aeroport FROM App\Entity\Aeroport aeroport WHERE aeroport.code LIKE 'CLR%'
+        // SELECT aeroport FROM App\Entity\Aeroport aeroport WHERE aeroport.code LIKE '%CLR%'
+        // SELECT aeroport FROM App\Entity\Aeroport aeroport WHERE aeroport.code LIKE :code
+        // SELECT aeroport FROM App\Entity\Aeroport aeroport WHERE aeroport.code LIKE %:code% // non!!!
+        // SELECT aeroport FROM App\Entity\Aeroport aeroport WHERE aeroport.code LIKE %'CLR'% // non!!! le % ne vas pas avant les ""
+        // Nous devons rajouter les % dans setParameter. Attention, il n'y a pas de ":" dans l'appel à cette fonction
+
     }
+
 }
